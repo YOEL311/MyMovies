ed0e0988b760b447123f50be6cf6fb5d355b50e7
YOEL311 <YOEL301@GMAIL.COM>
2020-09-06 10:41:14 +0300

    add utilities

---
+++ src/Utilities.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'c78332145219da2f5e664c92595c75af';
+export const PRE_IMAGE = 'https://image.tmdb.org/t/p/w185/';
+export const WEB_API_KEY =
+  '399342684527-nmaaolvbs1ks74ioq70t285kroulbn05.apps.googleusercontent.com';
